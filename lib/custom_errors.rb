208a1b71c1ea83d7b57815d7fbf564dfc705c49e
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-08 02:17:32 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -8,10 +8,10 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
-  raise PartnerError
-else
-    person.partner = self
-  end
+      raise PartnerError
+    else
+      person.partner = self
+    end
   end
 
   class PartnerError < StandardError
